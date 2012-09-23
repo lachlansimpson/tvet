1a2234875965a94f283cd72e8e1fcbb3a1c47d87
Lachlan Musicman <datakid@gmail.com>
2012-09-23 20:39:35 +1200

    made some mods, created the beginning of the applicant-> student object creation view

--- tafe/views.py
+++ tafe/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 
-from tafe.models import Timetable, Session, Course
+from tafe.models import Timetable, Session, Course, Student
 from tafe.forms import SessionRecurringForm, ApplicantSuccessForm
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect
@@ -25,6 +25,9 @@ def index(request):
 
 @login_required
 def timetable_weekly_view(request, slug):
+    '''
+    View of this week's timetable. 
+    '''
     timetable = get_object_or_404(Timetable, slug=slug)
     all_sessions = []
     
@@ -32,9 +35,9 @@ def timetable_weekly_view(request, slug):
 
     ''' For each day of the week '''
     for day in range(5):
-        ''' show the timetable for this week '''        
+        ''' To show the timetable for this week we get the date of this week's  Monday '''        
         weekday = last_monday + datetime.timedelta(day)
-        ''' all sessions is returned to the template - this is the complete data set'''
+        ''' all sessions is the dataset returned to the template'''
         all_sessions.append([])
         ''' weekdays is the daily list of sessions '''
         weekdays = []
@@ -53,6 +56,7 @@ def timetable_weekly_view(request, slug):
 
 @login_required
 def timetable_daily_view(request, year, month, day):
+    ''' Today's timetable  '''
     daily_sessions = []
     date = datetime.date(int(year), int(month), int(day))
 
@@ -64,6 +68,7 @@ def timetable_daily_view(request, year, month, day):
 
 @login_required
 def session_create(request):
+    ''' For each subject, we will create a session object for each day that it is taught '''
     if request.method == 'POST':
         form = SessionRecurringForm(request.POST)
         if form.is_valid():
@@ -103,6 +108,7 @@ def session_create(request):
 
 @login_required
 def session_view(request, year, month, day, slug):
+    ''' Show the details of the session '''
     req_date = datetime.date(int(year), int(month), int(day))
     session = get_object_or_404(Session, slug=slug, date=req_date)
 
@@ -110,14 +116,25 @@ def session_view(request, year, month, day, slug):
 
 @login_required
 def units_by_qualifications_view(request):
+    ''' Show all units or Subjects available for this Course'''
     courses = Course.objects.all().order_by('name')
     return render_to_response('tafe/units_by_qualifications.html',{'courses':courses})
 
 @login_required
 def applicant_success(request):
+    ''' All successful applicants will be turned into Student objects '''
     if request.method=='POST':
-        pass
-
+        form = ApplicantSuccessForm(request.POST)
+        if form.is_valid():
+            for applicant in form.cleaned_data['applicants']:
+                S = Student()
+                S.first_name = applicant.first_name
+                S.surname = applicant.surname
+                S.dob = applicant.dob
+                S.gender = applicant.gender
+                S.save()
+        else:
+            pass
     else:
         form = ApplicantSuccessForm()
 
