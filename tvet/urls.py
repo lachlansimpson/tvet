52013cc78aff65ec9d6870889d2d95f04b90ef74
Lachlan Musicman <datakid@gmail.com>
2012-10-26 15:47:28 +1200

    changes to tvet urls to solve the / issue

--- tvet/urls.py
+++ tvet/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', 'tafe.views.index'),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
-    
     url(r'^admin/', include(admin.site.urls)),
     url(r'^tafe/', include('tafe.urls')),
     url(r'^comments/',include('django.contrib.comments.urls')),
